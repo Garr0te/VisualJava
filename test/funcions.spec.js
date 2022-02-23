217621e9efc797a4768031c79ca7dd9d7dea4ff3
valldignalive <valldignalive@protonmail.com>
2022-02-23 23:26:27 +0100

    canvis en la funcio nom

--- test/funcions.spec.js
+++ test/funcions.spec.js
@@ -5,15 +5,15 @@ const llista = require('../llista')
 
 
 
-describe(" Test ", function() {
+describe(" Test ordenar per Nom", function() {
   describe("Check 1: ", function() {
     it("Ordenar per Nom EXPECT", function() {
       expect(funcions.ordenar_per_nom(llista.alumnes)).to.eql(llista.alumnesPerNom)
     })
 
-  it("Ordenar per Data ASSERT", function() {
+  it("Test ordenar per Data ", function() {
           for (var i = 0; i < llista.alumnes.length; i++) {
-            assert.equal(funcions.ordenar_per_nom(llista.alumnes)[i]['nom'], llista.alumnesPerNom[i]['nom'])
+            assert.equal(funcions.ordenar_per_data(llista.alumnes)[i]['nom'], llista.alumnesPerData[i]['nom'])
   }  
   });
 
